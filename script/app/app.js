bc376e9172ea21d24790e6f9a0197ca886cbb2ea
Cristian Zamar <cristianzamar@gmail.com>
2013-12-12 11:49:27 -0300

    definí IMAGE_RELOAD_ATTEMPTS y DOTS_PER_INCH

--- script/app/app.js
+++ script/app/app.js
@@ -1,5 +1,10 @@
 //Ext.onReady(function() {
 
+// pink tile avoidance
+OpenLayers.IMAGE_RELOAD_ATTEMPTS = 5;
+// make OL compute scale according to WMS spec
+OpenLayers.DOTS_PER_INCH = 25.4 / 0.28;
+
 GeoExt.Lang.set("es");
 
 var app = new gxp.Viewer({
